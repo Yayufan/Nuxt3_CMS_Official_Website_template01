73b0f4fe78b16d28953f5e832f9940c075cfdeb3
Yayufan <kamikazey2200@gmail.com>
2024-10-25 17:35:25 +0800

    update become-member-page

--- pages/become-member.vue
+++ pages/become-member.vue
@@ -10,11 +10,13 @@
             <article class="paragraph">
                 <h2 class="sub-title">會員類別</h2>
                 <div class="individual-member-box">
-                    <div clsss="common-label">個人會員</div>
+                    <div class="common-label">個人會員</div>
                     <p>凡贊同本會宗旨，年滿二十歲，填具入會申請書</p>
                 </div>
                 <div class="group-member-box"></div>
 
+
+                <div class="number-mask">1</div>
 
             </article>
 
@@ -32,10 +34,12 @@ import { ref, reactive } from 'vue'
 </script>
 
 <style scoped lang="scss">
+
 .common-seciton {
     width: $common-section-width;
     margin: $common-section-margin;
     font-family: $common-section-font-family;
+
 
     .common-title {
         color: $main-color;
@@ -48,12 +52,55 @@ import { ref, reactive } from 'vue'
     }
 
     .content-box {
-        background-color: aqua;
+        // background-color: aqua;
         width: 100%;
+        color: $main-content-color;
 
         /**這邊是處理通用段落、sub-title 和 內容 */
         .paragraph {
+            position: relative;
+            min-height: 100px;
+            border: 5px solid #E5E8E0;
+            border-radius: 80px 40px 40px 40px;
+            background-color: #fff;
+            padding: 1% 4%;
             margin-bottom: 3%;
+
+
+            .sub-title {
+                color: #56575E;
+                font-size: 1.3rem;
+            }
+
+
+            .number-mask {
+                font-size: 2rem;
+                background-color: #C5CEB6;
+                /* 背景顏色 */
+                color: white;
+                /* 文字顏色 */
+                border-radius: 50%;
+                /* 圓形 */
+                width: 2.5rem;
+                /* 圓形寬度 */
+                height: 2.5rem;
+                /* 圓形高度 */
+                display: inline-block;
+                /* 使其為行內塊元素 */
+                text-align: center;
+                /* 文字置中 */
+                line-height: 2.5rem;
+                /* 行高等於圓形高度以置中 */
+                position: absolute;
+                /* 絕對定位 */
+                left: 0;
+                /* 相對於 li 的位置 */
+                top: 7%;
+                /* 垂直置中 */
+                transform: translateY(-50%);
+                /* 調整位置至垂直中心 */
+            }
+
         }
 
     }
--- pages/purpose.vue
+++ pages/purpose.vue
@@ -272,7 +272,7 @@ const getOtherList = () => {
     .content-box {
         // background-color: aqua;
         width: 100%;
-        color: #6D6D6D;
+        color: $main-content-color;
 
         /**這邊是處理通用段落、sub-title 和 內容 */
         .paragraph {
