84bce22f27d281dfbe6db89f1e5d192c77fc3dbb
Yayufan <kamikazey2200@gmail.com>
2024-10-25 20:56:07 +0800

    finish  feature/become-member page

--- pages/purpose.vue
+++ pages/purpose.vue
@@ -81,7 +81,7 @@
             <article class="paragraph ">
                 <h2 class="sub-title">協會章程</h2>
                 <div class="content regulation-box">
-                    <a href="@/assets/files/regulation_1071005.pdf" download >社團法人章程-1071005修正版.pdf</a>
+                    <a href="/files/regulation_1071005.pdf" download >社團法人章程-1071005修正版.pdf</a>
                 </div>
 
             </article>
